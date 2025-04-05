f600e8661762bf23c63ed03046537c86b4da5eb4
harry.sjh <harry.sjh@foxmail.com>
2025-04-05 23:55:14 +0800

    npm publish

---
+++ publish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 终止执行遇到错误的命令
+set -e
+
+echo "🟢 添加更改..."
+git add .
+
+echo "🟡 提交更改..."
+git commit -m "npm publish"
+
+echo "🟠 升级版本号（patch）..."
+npm version patch
+
+echo "🔵 推送到 GitHub..."
+git push
+
+echo "✅ 已完成推送，GitHub Action 将被触发"
