855441b824a16259ed467e090de54b132fd76e30
harry.sjh <harry.sjh@foxmail.com>
2025-06-13 22:14:48 +0800

    npm publish

--- publish.sh
+++ publish.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-
-# 终止执行遇到错误的命令
 set -e
 
 echo "🟢 添加更改..."
 git add .
 
 echo "🟡 提交更改..."
-git commit -m "npm publish"
+git commit -m "npm publish" || echo "⚠️ 没有更改可提交"
 
 echo "🟠 升级版本号（patch）..."
 npm version patch
 
-# npm version patch: Increments the patch version, creates a Git tag, and commits.
-# npm version minor: Increments the minor version.
-# npm version major: Increments the major version.
-
 echo "🔵 推送到 GitHub..."
-git push
+git push origin HEAD:main --force-with-lease
 
 echo "✅ 已完成推送，GitHub Action 将被触发"
+
+read -p "按任意键退出..."
+
